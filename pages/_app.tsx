bb6802745f11c122c4618ff01bb1343299856fa4
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-06-01 15:03:36 +0300

    feat: react-query pkg install setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import type { NextPage } from "next"
 import type { AppProps } from "next/app"
+import { QueryClient, QueryClientProvider } from "react-query"
 import "../styles/globals.css"
 
+const queryClient = new QueryClient()
+
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
 
 export default App
