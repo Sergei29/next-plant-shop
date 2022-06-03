708c790d769ab278cd609d1f49a0754220d31c96
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-06-03 16:59:37 +0300

    refactor: fn body removed

--- components/GoogleLogin/index.tsx
+++ components/GoogleLogin/index.tsx
@@ -2,18 +2,16 @@ import React from "react"
 import { NEXT_PUBLIC_CMS_API } from "../../constants"
 import GoogleIcon from "./components/GoogleIcon"
 
-const GoogleLogin = (): JSX.Element => {
-  return (
-    <button
-      className="flex items-center px-2 text-lg bg-white border-2 border-gray-500 rounded-md shadow outline-none hover:bg-slate-50 hover:border-slate-400 focus:outline-none"
-      onClick={() =>
-        (window.location = `${NEXT_PUBLIC_CMS_API}/connect/google` as any)
-      }
-    >
-      <GoogleIcon className="w-6 h-6 mr-3 text-gray-900 fill-current" />
-      Google login
-    </button>
-  )
-}
+const GoogleLogin = (): JSX.Element => (
+  <button
+    className="flex items-center px-2 text-lg bg-white border-2 border-gray-500 rounded-md shadow outline-none hover:bg-slate-50 hover:border-slate-400 focus:outline-none"
+    onClick={() =>
+      (window.location = `${NEXT_PUBLIC_CMS_API}/connect/google` as any)
+    }
+  >
+    <GoogleIcon className="w-6 h-6 mr-3 text-gray-900 fill-current" />
+    Google login
+  </button>
+)
 
 export default GoogleLogin
