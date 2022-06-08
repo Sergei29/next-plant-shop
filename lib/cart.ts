848e660d73fcc12da1d49950dc389db9f459a0c1
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-06-08 21:15:53 +0300

    feat: added js-doc comments to lib utils

--- lib/cart.ts
+++ lib/cart.ts
@@ -6,11 +6,23 @@ import {
   Cart,
 } from "../types"
 
+/**
+ * @description util: strips product object from un-necessary properties
+ * @param {number} id product ID
+ * @param {object} attributes product data
+ * @returns {object} only the necerssary product properties
+ */
 export const stripCartProduct = ({
   id,
   attributes: { title, price },
 }: CartProduct) => ({ productId: id, productTitle: title, productPrice: price })
 
+/**
+ * @description util: strips cart-item object from un-necessary properties
+ * @param {number} id cart-item ID
+ * @param {object} attributes cart-item data
+ * @returns {object} only the necerssary cart-item properties
+ */
 export const stripCartItem = ({
   id,
   attributes: { quantity, product },
@@ -21,9 +33,19 @@ export const stripCartItem = ({
   total: product.data.attributes.price * quantity,
 })
 
+/**
+ * @description util: calculates total price of all items in the cart
+ * @param {Array} cartItems cart-items list
+ * @returns {number} total price
+ */
 export const getCartTotal = (cartItems: CartItemFormatted[]) =>
   cartItems.reduce((grandTotal, current) => (grandTotal += current.total), 0)
 
+/**
+ * @description util: formatting cart items
+ * @param {Array} items list of cart items from CMS
+ * @returns {Array}  list of cart items without possible duplicates
+ */
 export const formatCartItems = (
   items: CartItemFormatted[]
 ): CartItemFormatted[] => {
@@ -44,6 +66,11 @@ export const formatCartItems = (
   return Object.values(cache)
 }
 
+/**
+ * @description util: formatting cart payload received from CMS API
+ * @param {object} data API response payload
+ * @returns {object} formatted user's shopping cart
+ */
 export const formatCartPayload = ({ data }: ApiCartPayloadType): Cart => {
   const items = formatCartItems(data.map(stripCartItem))
   return {
--- lib/products.ts
+++ lib/products.ts
@@ -10,6 +10,12 @@ import {
 import { CMS_API } from "../constants"
 import { fetchData } from "./api"
 
+/**
+ * @description util: formatting the product payload received from CMS API
+ * @param {number} id product ID
+ * @param {object} attributes product info
+ * @returns {object} formatted product, flattened structure, only necessary properties
+ */
 export const formatRawProduct = ({
   id,
   attributes,
@@ -22,12 +28,27 @@ export const formatRawProduct = ({
   },
 })
 
+/**
+ * @description util: format products list payload received from CMS API
+ * @param {object} apiPayload products list API response payload
+ * @returns {Array} formatted products list
+ */
 export const formatProductsApiPayload = (
   apiPayload: ApiPayloadProductsList
 ): Product[] => apiPayload.data.map(formatRawProduct)
 
+/**
+ * @description  util: strips image object from un-necessary properties
+ * @param {object} imageData image info nested object
+ * @returns {object} image data only necessary properties
+ */
 const stripPicture = ({ id, url }: ImageData): ImageDataShort => ({ id, url })
 
+/**
+ * @description  util: strips product object from un-necessary properties
+ * @param {object} productData product info nested object
+ * @returns {object} product data only necessary properties
+ */
 const stripProduct = ({
   id,
   title,
@@ -40,9 +61,19 @@ const stripProduct = ({
   picture: stripPicture(picture),
 })
 
+/**
+ * @description util: display user friendly price
+ * @param {number} amount price amount
+ * @param {string} currency currency ( optional, defaults to $ )
+ * @returns {string} currency + price decimals display
+ */
 export const displayPrice = (amount: number, currency = "$") =>
   currency + amount.toFixed(2)
 
+/**
+ * @description fetching the products list from CMS API
+ * @returns {Promise<Array>} promise resolving to a list of formatted products
+ */
 export const getProducts = async (): Promise<ProductShort[]> => {
   const productsRaw = await fetchData<ApiPayloadProductsList>(
     `${CMS_API}/products?populate=*`
@@ -50,6 +81,11 @@ export const getProducts = async (): Promise<ProductShort[]> => {
   return formatProductsApiPayload(productsRaw).map(stripProduct)
 }
 
+/**
+ * @description fetching product by ID from CMS API
+ * @param {string} id product ID ( numeric string value )
+ * @returns {Promise<Object>} promise resolving to formatted product
+ */
 export const getProductById = async (id: string): Promise<Product> => {
   const productRaw = await fetchData<ApiPayloadProductDetails>(
     `${CMS_API}/products/${id}?populate=*`
@@ -57,6 +93,10 @@ export const getProductById = async (id: string): Promise<Product> => {
   return formatRawProduct(productRaw.data)
 }
 
+/**
+ * @description generates product static paths for product pages
+ * @returns {Promise<Object>} promise, resolving to the list of paths for SSG `getStaticPaths`
+ */
 export const generateProductsStaticPaths = async () => {
   const products = await getProducts()
   return products.map(({ id }) => ({
